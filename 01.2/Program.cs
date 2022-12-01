005550e9d78538e4dd997f10a50e7c323f497b27
Thomas Heder <thomasheder@sogeti.com>
2022-12-01 13:18:30 +0100

    .

--- 01.2/Program.cs
+++ 01.2/Program.cs
@@ -3,7 +3,7 @@
     static void Main()
     {
         var lines = File.ReadLines("in.txt").ToArray();
-        List<int> groups = new List<int>();
+        var groups = new List<int>();
 
         int curr = 0;
         for (int i = 0; i < lines.Length; i++)
