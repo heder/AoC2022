be1f72f20c707e6515f46e1c04231a2e3bbd626a
Thomas Heder <thomasheder@sogeti.com>
2022-12-03 11:05:51 +0100

    3

---
+++ 04.1/Program.cs
@@ -0,0 +1,46 @@
+﻿class Program
+{
+    static readonly Dictionary<char, int> priorities = new();
+
+    static void Main()
+    {
+        int i = 1;
+        for (char letter = 'a'; letter <= 'z'; letter++)
+        {
+            priorities.Add(letter, i);
+            i++;
+        }
+        for (char letter = 'A'; letter <= 'Z'; letter++)
+        {
+            priorities.Add(letter, i);
+            i++;
+        }
+
+        var lines = File.ReadLines("in.txt").ToArray();
+
+        var rucksacks = new List<Rucksack>();
+        foreach (var item in lines)
+        {
+            rucksacks.Add(new Rucksack(item));
+        }
+
+        var x = rucksacks.Sum(f => f.Prio);
+
+        Console.WriteLine(x);
+        Console.ReadKey();
+    }
+
+    class Rucksack
+    {
+        public Rucksack(string content)
+        {
+            string a = content[..(content.Length / 2)];
+            string b = content[(content.Length / 2)..];
+
+            var common = a.Intersect(b);
+            Prio = Program.priorities[common.Single()];
+        }
+
+        public int Prio { get; set; }
+    }
+}
---
+++ 04.2/Program.cs
@@ -0,0 +1,41 @@
+﻿
+class Program
+{
+    static readonly Dictionary<char, int> priorities = new();
+
+    static void Main()
+    {
+        int i = 1;
+        for (char letter = 'a'; letter <= 'z'; letter++)
+        {
+            priorities.Add(letter, i);
+            i++;
+        }
+        for (char letter = 'A'; letter <= 'Z'; letter++)
+        {
+            priorities.Add(letter, i);
+            i++;
+        }
+
+        var prios = new List<int>();
+        var lines = File.ReadLines("in.txt").ToArray();
+
+        i = 0;
+        while (i < lines.Length)
+        {
+            string a = lines[i];
+            string b = lines[i + 1];
+            string c = lines[i + 2];
+
+            var prio = a.Intersect(b.Intersect(c));
+            prios.Add(Program.priorities[prio.Single()]);
+
+            i += 3;
+        }
+
+        var x = prios.Sum();
+
+        Console.WriteLine(x);
+        Console.ReadKey();
+    }
+}
