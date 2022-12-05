c39c96227679b2b67cb5e195bab982f31792f748
Thomas Heder <thomas.heder@gmail.com>
2022-12-05 07:27:17 +0100

    5

---
+++ 05.2/Program.cs
@@ -0,0 +1,40 @@
+﻿class Program
+{
+    static void Main()
+    {
+        var lines = File.ReadLines("in.txt");
+        int count = 0;
+
+        foreach (var item in lines)
+        {
+            var s = item.Split('-', ',');
+            var a = new MyRange(Convert.ToInt32(s[0]), Convert.ToInt32(s[1]));
+            var b = new MyRange(Convert.ToInt32(s[2]), Convert.ToInt32(s[3]));
+            var c = new MyRange(Math.Min(a.Start, b.Start), Math.Max(a.End, b.End));
+
+            if (c.Length() <= a.Length() + b.Length())
+            {
+                count++;
+            }
+        }
+
+        Console.WriteLine(count);
+        Console.ReadKey();
+    }
+
+    public class MyRange
+    {
+        public MyRange(int s, int e)
+        {
+            Start = s; End = e;
+        }
+
+        public int Start { get; set; }
+        public int End { get; set; }
+
+        public int Length()
+        {
+            return End - Start;
+        }
+    }
+}
