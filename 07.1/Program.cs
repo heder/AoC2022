51453911db0fe11893b71090ba1e2dbcb1d7cb5f
Thomas Heder <thomas.heder@gmail.com>
2022-12-07 07:15:10 +0100

    7

---
+++ 07.1/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+    static void Main()
+    {
+        var line = File.ReadAllText("in.txt").ToArray();
+
+        for (int i = 0; i < line.Length; i++)
+        {
+            if (line[i..].Take(4).Distinct().Count() == 4)
+            {
+                Console.WriteLine(i + 4);
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ 07.2/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+    static void Main()
+    {
+        var line = File.ReadAllText("in.txt").ToArray();
+
+        for (int i = 0; i < line.Length; i++)
+        {
+            if (line[i..].Take(14).Distinct().Count() == 14)
+            {
+                Console.WriteLine(i + 14);
+                Console.ReadKey();
+            }
+        }
+    }
+}
