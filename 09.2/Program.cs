a25affabc1445c5ca257e3fce0b4b9bd013f761d
Thomas Heder <thomasheder@sogeti.com>
2022-12-09 14:01:27 +0100

    9

--- 09.2/Program.cs
+++ 09.2/Program.cs
@@ -1,29 +1,22 @@
-﻿using System.Xml.Serialization;
-
+﻿
 class Program
 {
     static int yLen = 1000;
     static int xLen = 1000;
     static char[,] world;
     static bool[,] visitedByTail;
-    //static Pos relative;
     static Pos[] ropePositions;
 
 
     static void Main()
     {
-        //relative = new Pos(500, 500);
-
         ropePositions = new Pos[10];
-
         for (int i = 0; i < 10; i++)
         {
             ropePositions[i] = new Pos(500, 500);
         }
-
         
         visitedByTail = new bool[xLen, yLen];
-        //visitedByTail[500, 500] = true;
 
         string[] lines = File.ReadAllLines("in.txt").ToArray();
 
@@ -31,10 +24,6 @@ class Program
         {
             var tokens = line.Split(' ');
             Move(tokens[0], Convert.ToInt32(tokens[1]));
-
-            //DumpState();
-
-            
         }
 
         yLen = lines.Length;
@@ -115,8 +104,6 @@ class Program
                 MovePos(pos, "D");
             }
         }
-
-        //visitedByTail[pos.X, pos.Y] = true;
     }
 
 
@@ -167,23 +154,6 @@ class Program
         Console.Write(Environment.NewLine);
     }
 
-
-    //private static void DumpWorld(int xmin, int xmax, int ymin, int ymax)
-    //{
-    //    for (int y = ymax; y >= ymin; y--)
-    //    {
-    //        for (int x = xmin; x <= xmax; x++)
-    //        {
-    //            Console.Write(visitedByTail[x, y] ? "#" : ".");
-    //        }
-
-    //        Console.Write(Environment.NewLine);
-    //    }
-
-    //    Console.Write(Environment.NewLine);
-    //}
-
-
     class Pos
     {
         public Pos(int x, int y)
@@ -194,54 +164,4 @@ class Program
         public int X { get; set; }
         public int Y { get; set; }
     }
-
-
-
-
-
-    //private static int IsVisible(int xin, int yin)
-    //{
-    //    int treeHeight = forest[xin, yin];
-
-    //    var uVec = new List<int>();
-    //    var dVec = new List<int>();
-    //    var lVec = new List<int>();
-    //    var rVec = new List<int>();
-
-    //    // (Y-) (up)
-    //    for (int y = yin - 1; y >= 0; y--)
-    //    {
-    //        uVec.Add(forest[xin, y]);
-    //    }
-
-    //    // Y+ (down)
-    //    for (int y = yin + 1; y < yLen; y++)
-    //    {
-    //        dVec.Add(forest[xin, y]);
-    //    }
-
-    //    // X- (left)
-    //    for (int x = xin - 1; x >= 0; x--)
-    //    {
-    //        lVec.Add(forest[x, yin]);
-    //    }
-
-    //    // X+ (right)
-    //    for (int x = xin + 1; x < xLen; x++)
-    //    {
-    //        rVec.Add(forest[x, yin]);
-    //    }
-
-    //    if (uVec.All(f => f < treeHeight) ||
-    //        dVec.All(f => f < treeHeight) ||
-    //        lVec.All(f => f < treeHeight) ||
-    //        rVec.All(f => f < treeHeight))
-    //    {
-    //        return 1;
-    //    }
-    //    else
-    //    {
-    //        return 0;
-    //    }
-    //}
 }
